701b11115f2713e3a3eb14ad9f44108c6243bdcd
Foo Yong Qin <yongqin3759@gmail.com>
2022-08-03 00:50:04 +0800

    added loaders and minifier

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import 'core-js/stable/index.js'
 import 'regenerator-runtime/runtime.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,24 @@ const config = {
     path: path.resolve(__dirname, 'build'),
     filename: 'main.js'
   },
+  devServer: {
+    static: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-react'],
+          presets: ['@babel/preset-env','@babel/preset-react'],
         },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
